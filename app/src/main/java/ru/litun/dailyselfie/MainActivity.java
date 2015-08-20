28a39ffebe21ecae78053e51cdc7e68fcbbdb993
Aleksander <litunone@gmail.com>
2015-08-21 02:48:12 +0300

    Action bar added, common style changes

--- app/src/main/java/ru/litun/dailyselfie/MainActivity.java
+++ app/src/main/java/ru/litun/dailyselfie/MainActivity.java
@@ -2,6 +2,7 @@ package ru.litun.dailyselfie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_camera) {
             return true;
         }
 
